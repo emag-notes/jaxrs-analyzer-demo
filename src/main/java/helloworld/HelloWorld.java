46c127278ee102bc86ccde81abc573c8a26a82c8
emag <lanabe.lanabe@gmail.com>
2016-12-25 23:14:53 +0900

    initial

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class HelloWorld {
+
+  /**
+   * Hello, World!
+   */
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public String hello(@QueryParam("message") String message) {
+    return String.format("{\"message\" : \"Hello, %s\"}", message);
+  }
+
+}
